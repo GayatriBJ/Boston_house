47a6f133f3018820fe59597883543f0e8229a0d8
GayatriBJ <125629830+GayatriBJ@users.noreply.github.com>
2023-07-28 23:07:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-MODEL_FILE_PATH = "static/model/bostondata_dt.pkl"
+MODEL_FILE_PATH = "static/MODEL/bostondata_dt.pkl"
 
-SCALER_FILE_PATH = "static/model/normal_scaler.pkl"
+SCALER_FILE_PATH = "static/MODEL/normal_scaler.pkl"
